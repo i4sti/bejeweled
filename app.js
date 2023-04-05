5e7a1228f2e3f6f29ceb7864dcddb8e0cc3a2fcd
Isti <h161882@stud.u-szeged.hu>
2023-04-06 00:29:50 +0200

    kis haladás

--- app.js
+++ app.js
@@ -2,6 +2,57 @@
 const welcomeScreen = document.getElementById('welcome-screen');
 const gameScreen = document.getElementById('game-screen');
 
+function submitName() {
+    const nameInput = document.getElementById('name');
+    const name = nameInput.value.trim();
+
+    if (name === '') {
+        alert('Please enter a name.');
+        return;
+    }
+
+    let highScores = JSON.parse(localStorage.getItem('highScores')) || {};
+
+    if (highScores.hasOwnProperty(name)) {
+        alert('Name already exists. Please enter a different name.');
+        return;
+    }
+
+
+    highScores[name] = score;
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
+    // updateScoreList();
+    nameInput.value = '';
+}
+//display scores
+function displayScores() {
+    const scoreList = document.getElementById('score-list');
+    const scores = Object.entries(localStorage)
+        .filter(([key, value]) => !isNaN(value))
+        .map(([key, value]) => ({ name: key, score: parseInt(value) }))
+        .sort((a, b) => b.score - a.score);
+
+    // Clear previous scores from the list
+    scoreList.innerHTML = '';
+
+    // Append each score to the list as a new item
+    scores.forEach((score) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${score.name}: ${score.score}`;
+        scoreList.appendChild(listItem);
+    });
+}
+
+
+
+
+
+
+//score
+
+
+
 // Get a reference to the play button
 const playButton = document.getElementById('play-button');
 
@@ -14,6 +65,7 @@ playButton.addEventListener('click', () => {
     // Show the game screen
     gameScreen.style.display = 'block';
 });
+
 
 //Game
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -47,7 +99,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     function createBoard(){
         for (let i = 0; i < width*width; i++) {
             const  square = document.createElement('div')
-            square.setAttribute('draggable', true)
             square.setAttribute('id', i)
             // square.setAttribute('class', 'rotate')
             let randomColor = Math.floor(Math.random() * jewelColors.length)
@@ -119,41 +170,59 @@ document.addEventListener('DOMContentLoaded', ()=>{
             hush.play()
 
 
-            console.log("sor "+checkRowForThree())
-            console.log("oszlop " + checkColumnForThree())
+
             setTimeout(()=>{
-                if (checkForMatches()) {
-                    console.log("jó")
+                let row = checkForRow()
+                let vertical = checkForVertical()
+
+                let first = (firstId === row || firstId === vertical)
+                let first2 = (firstId+1 === row || firstId+width === vertical) || (firstId-1 === row || firstId-width === vertical)
+                let first3 =(firstId+2 === row || firstId+2*width === vertical) || (firstId-2 === row || firstId-2*width === vertical)
+                let second = (secondId === row || secondId === vertical)
+                let second2 = (secondId+1 === row || secondId+width === vertical) || (secondId-1 === row || secondId-width === vertical)
+                let second3 = (secondId+2 === row || secondId+2*width === vertical) || (secondId-2 === row || secondId-2*width === vertical)
+
+                let elso = first || first2 || first3
+                let masodik = second || second2 || second3
+                console.log("1.1"+first)
+                console.log("1.2"+first2)
+                console.log("1.3"+first3)
+                console.log("2.1"+second)
+                console.log("2.2"+second2)
+                console.log("2.3"+second3)
+                if ((row !== -1 || vertical !== -1) && (elso || masodik) ) {
+                    console.log("row " + row)
+                    console.log("vertiacl " + vertical)
                     moveDown()
                     // There is a match, so leave the new colors of the gems as they are
+                } else{
+                    console.log("nem volt match")
+                    const tempBackgroundImage = firstSquare.style.backgroundImage;
+                    firstSquare.style.backgroundImage = secondSquare.style.backgroundImage;
+                    secondSquare.style.backgroundImage = tempBackgroundImage;
+
+                    const tempId = firstSquare.id;
+                    firstSquare.id = secondSquare.id;
+                    secondSquare.id = tempId;
+
+                    squares[firstId].style.backgroundImage = firstColor;
+                    const hush = new Audio('sounds/no-good.mp3');
+                    hush.volume=0.7
+                    hush.play()
+                    // reset first and second square variables
+                    firstSquare = null
+                    secondSquare = null
                 }
 
-                // reset first and second square variables
-                firstSquare = null
-                secondSquare = null
+
             },100)
-            console.log(squares)
 
         }else {
             const hush = new Audio('sounds/no-good.mp3');
             hush.volume=0.7
             hush.play()
         }
-
-
-
     }
-    // setInterval(() => {
-    //     for(let i = 0; i < squaresclick.length; i++){
-    //         if(squaresclick[i] !== squaresdrag[i]){
-    //             console.log(`Element ${i+1} is different: ${squaresclick[i]} !== ${squaresdrag[i]}`);
-    //         }else {
-    //             console.log("ugyanaz1")
-    //         }
-    //     }
-    //     console.log("ugyanaz2")
-    //
-    // }, 1000);
 
     function checkForAdjacent(firstsquare, secondsquare) {
         const index1 = squares.indexOf(firstsquare)
@@ -163,81 +232,78 @@ document.addEventListener('DOMContentLoaded', ()=>{
         const isAdjacentRow = (diff === 1 && Math.floor(index1 / width) === Math.floor(index2 / width))
         return isAdjacentCol || isAdjacentRow
     }
-    function checkForMatches() {
-        return checkColumnForThree() || checkRowForThree()
-    }
-
-    //essenek le ha eltünt
+    //movedown if cleared
     function moveDown(){
-        for (let i = 0; i < 55; i++) {
-            if (squares[i + width].style.backgroundImage === ''){
+        let allFilled = false
 
-                squares[i+width].style.backgroundImage = squares[i].style.backgroundImage
-                squares[i].style.backgroundImage = ''
-                const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
-                const isFirstRow = firstRow.includes(i)
-                if (isFirstRow && squares[i].style.backgroundImage === ''){
-                    console.log("lekellcsuszni")
-                    let randomColor = Math.floor(Math.random() * jewelColors.length)
-                    squares[i].style.backgroundImage = jewelColors[randomColor]
+            for (let i = 0; i < 55; i++) {
+                if (squares[i + width].style.backgroundImage === ''){
+
+                    squares[i+width].style.backgroundImage = squares[i].style.backgroundImage
+                    squares[i].style.backgroundImage = ''
+                    const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+                    const isFirstRow = firstRow.includes(i)
+                    if (isFirstRow && squares[i].style.backgroundImage === ''){
+                        console.log("lekellcsuszni")
+                        let randomColor = Math.floor(Math.random() * jewelColors.length)
+                        squares[i].style.backgroundImage = jewelColors[randomColor]
+                    }
+
                 }
             }
-        }
+
+
     }
 
-    //megnézni hogy mikor van 3 egyforma egy sorban
-    function checkRowForThree() {
-        for (let i = 0; i < 61; i++) {
-            let rowOfThree = [i, i + 1, i + 2]
-            let decidedColor = squares[i].style.backgroundImage
-            const isBlank = squares[i].style.backgroundImage === ''
+
+    function checkForRow() {
+        let row = -1;
+        for (let i = 0; i < squares.length-2; i++) {
+            const currentGem = squares[i];
+            const nextGem = squares[i + 1];
+            const nextNextGem = squares[i + 2];
 
 
-            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
-            if (notValid.includes(i)) {
-                console.log("tovább lép")
-                continue
+            const isMatch = currentGem.style.backgroundImage === nextGem.style.backgroundImage
+                && nextGem.style.backgroundImage === nextNextGem.style.backgroundImage;
+
+            if (isMatch && i % width < width - 2) {
+                row = i;
+                // Clear the matched gems
+                // currentGem.style.backgroundImage = '';
+                // nextGem.style.backgroundImage = '';
+                // nextNextGem.style.backgroundImage = '';
             }
-            console.log("blank "+isBlank)
-            if (rowOfThree.every(index => squares[index].style.backgroundImage === decidedColor && !isBlank)) {
-                console.log("belép ha jó")
-                score += 5
-                scoreDisplay.innerHTML = score
-                rowOfThree.forEach(index => {
-                    squares[index].style.backgroundImage = ''
-
-                })
-                console.log(decidedColor, "jó nagyon sor")
-                return true
-            } else {
-                return false
-            }
-
         }
+
+        return row;
     }
-    //megnézni hogy mikor van 3 egyforma egy oszlopban
-    function checkColumnForThree(){
-        for (let i = 0; i < 47; i++) {
-            let columnOfThree = [i, i+width, i+width*2]
-            let decidedColor = squares[i].style.backgroundImage
-            const isBlank = squares[i].style.backgroundImage === ''
 
-            if (columnOfThree.every(index => squares[index].style.backgroundImage === decidedColor && !isBlank)){
-                score += 5
-                scoreDisplay.innerHTML = score
-                columnOfThree.forEach(index => {
-                    squares[index].style.backgroundImage = ''
+    function checkForVertical() {
+        let vertical = -1;
+        for (let i = 0; i < squares.length-1; i++) {
+            if (
+                i + width * 2 < squares.length &&
+                squares[i].style.backgroundImage === squares[i + width].style.backgroundImage &&
+                squares[i].style.backgroundImage === squares[i + width * 2].style.backgroundImage
+            ) {
+                console.log('Vertical match at ' + i);
+                vertical = i;
 
-                })
-                console.log(decidedColor, "jó nagyon oszlop")
-
-                return true
-            }else {
-                return false
+                // Clear the matched gems
+                // squares[i].style.backgroundImage = '';
+                // squares[i + width].style.backgroundImage = '';
+                // squares[i + width * 2].style.backgroundImage = '';
             }
-
         }
+
+        return vertical;
     }
+
+    // setInterval(moveDown,500)
+    // setInterval(checkForVertical,500)
+    // setInterval(checkForRow,500)
+
 
 
 
